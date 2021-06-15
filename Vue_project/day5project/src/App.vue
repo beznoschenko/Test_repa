40887f5d3789dfdc05d848601fc8c4ee1a0544e7
beznoshchenkova <Anirva1994@gmail.com>
2021-06-15 15:22:57 +0300

    Add product cards

--- Vue_project/day5project/src/App.vue
+++ Vue_project/day5project/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper">
-    <span>First Name</span>
+    <!-- <span>First Name</span>
     <custom-input v-model="inputText.f_name" :maxlength="5"/>
     <br />
     <span>Last Name</span>
@@ -13,14 +13,16 @@
     <custom-input v-model="inputText.age" :max="maxVal" :min="minVal" number />
 
     <br />
-    {{ fullInformation }}
-  </div>
+    {{ fullInformation }} -->
+    <product-card v-for="(item, index) in product" :key="index" :dataProduct="item" v-model="checkData" />
+    </div>
 </template>
 
 <script>
 import CustomInput from "./components/custom_input.vue";
 import CustomCheckbox from "./components/custom_checkbox.vue";
 import CustomRadioButtonGroup from "./components/custom_radiogroup.vue";
+import ProductCard from "./components/product_card.vue";
 
 export default {
   name: "App",
@@ -28,22 +30,37 @@ export default {
     CustomInput,
     CustomCheckbox,
     CustomRadioButtonGroup,
+    ProductCard
   },
   data() {
     return {
-      maxVal: 100,
-      minVal: 0,
-      inputText: { f_name: "", l_name: "", gender: "", age: 0 },
-      value: false,
-      radioCheck: true,
+      // maxVal: 100,
+      // minVal: 0,
+      // inputText: { f_name: "", l_name: "", gender: "", age: 0 },
+      // value: false,
+      // radioCheck: true,
       checkData: {},
-      radio: [{ label: "Мужчина" }, { label: "Женщина" }]
+      // radio: [{ label: "Мужчина" }, { label: "Женщина" }]
+      product: [
+        {
+          name: "Samsung Galaxy Note 20 Ultra",
+          price: 31999,
+          discount_price: 28800,
+          discount_percent: 10,
+          image: "https://content2.rozetka.com.ua/goods/images/big/141334395.jpg",
+        },
+        {
+          name: "Samsung Galaxy A02s",
+          price: 3799,
+          image: "https://content.rozetka.com.ua/goods/images/big/178061571.jpg",
+        },
+      ],
     };
   },
   computed: {
-    fullInformation() {
-      return this.inputText.gender+':'+this.inputText.f_name+' '+this.inputText.l_name+', '+this.inputText.age;
-    },
+    // fullInformation() {
+    //   return this.inputText.gender+':'+this.inputText.f_name+' '+this.inputText.l_name+', '+this.inputText.age;
+    // },
   },
 };
 </script>
@@ -53,8 +70,7 @@ export default {
   width: 100%;
   height: 100%;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  flex-direction: row;
+  align-items: left;
 }
 </style>
---
+++ Vue_project/day5project/src/components/product_card.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="product-property">
+      <!-- Discount -->
+      <div
+        v-if="!!dataProduct.discount_price"
+        class="product-property__discount-percent"
+      >
+        {{ dataProduct.discount_percent }}%
+      </div>
+    <!-- Image -->
+    <div class="product-property__image">
+      
+      <img
+       :src="dataProduct.image"
+        :alt="dataProduct.name"
+        width="auto"
+        height="300px"
+      />
+    </div>
+    <!-- Name -->
+    <span class="product-property__row-name">{{ dataProduct.name }}</span>
+    <!-- price -->
+    <span class="product-property__row-price">
+      Price:<span v-if="!!dataProduct.discount_price"
+        >{{ dataProduct.discount_price }}
+      </span>
+      <span
+        :class="[!!dataProduct.discount_price ? 'product-property__discount' : '']"
+        >{{ dataProduct.price }}
+      </span>
+    </span>
+
+    <button class="product-property__button" @click="buy_product">
+      <i class="fas fa-shopping-bag"></i> Buy
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+  props: {
+    dataProduct: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    buy_product() {
+      this.$emit("input", this.dataProduct);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.product-property {
+  width: 20rem;
+  border: 1px solid #283747;
+  border-radius: 10px;
+  padding: 15px 5px;
+  margin: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  &__row {
+    display: inline-block;
+    color: #1c2833;
+    min-width: 0;
+    font-family: "Sylfaen", serif;
+    &-name {
+      font-size: 14pt;
+      font-weight: bold;
+      margin-bottom: 5px;
+    }
+    &-price {
+      font-size: 12pt;
+      margin-bottom: 15px;
+    }
+  }
+  &__discount {
+    text-decoration: line-through;
+  }
+  &__image {
+    width: auto;
+    height: 300px;
+    position: relative;
+  }
+  &__discount-percent {
+    background-color: rgba(255, 102, 0, 0.8);
+    right: 0;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+  }
+  &__button {
+    background-color: #00bc52;
+    border: 1px solid #00ad4c;
+    color: white;
+    font-size: 12pt;
+    padding: 10px 25px;
+    border-radius: 5px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19);
+    &:hover {
+      background-color: #00ad4c;
+    }
+  }
+  &:hover {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19);
+  }
+}
+</style>
